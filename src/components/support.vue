9aa3ec004d397d70c109066c5b6c28672532e8eb
Benoit K <28750720+BekaD12@users.noreply.github.com>
2023-08-25 11:56:37 +0200

    Update support.vue

--- src/components/support.vue
+++ src/components/support.vue
@@ -1,6 +1,6 @@
 <script setup>
 const engage = ['Thresh', 'Taric', 'Amumu', 'Nautilus', 'Blitzcrank']
-const pressure = ['Senna', 'Zyra', 'Lux']
+const pressure = ['Senna', 'Xerath', 'Zyra']
 const enchanter = ['Karma', 'Seraphine']
 
 function link(i) {
